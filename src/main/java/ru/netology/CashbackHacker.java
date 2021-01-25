4ae9480ac8d2a7aa5731a20a8cf439235b2ffe5d
IldarGabi <IldarGabi@gmail.com>
2021-01-26 01:08:48 +0300

    Used TestNG, update code and add tests

--- src/main/java/ru/netology/CashbackHacker.java
+++ src/main/java/ru/netology/CashbackHacker.java
@@ -1,9 +1,11 @@
 package ru.netology;
 
 public class CashbackHacker {
-    private final int boundary = 1000;
-
+    public final int boundary = 1000;
     public int remain(int amount) {
+        if (amount==1000){
+            return (boundary - amount) % boundary;
+        }
         return boundary - amount % boundary;
-    }
+}
 }
