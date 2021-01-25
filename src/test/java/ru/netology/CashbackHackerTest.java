cefbab2ba79df1d0ae9b375995a4344520228c8d
IldarGabi <IldarGabi@gmail.com>
2021-01-26 01:15:47 +0300

    Used JUnit4, add tests

---
+++ src/test/java/ru/netology/CashbackHackerTest.java
@@ -0,0 +1,37 @@
+package ru.netology;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackerTest {
+    CashbackHacker cashbackHacker = new CashbackHacker();
+
+    @Test
+    public void UnderBoundary() {
+        int actual = cashbackHacker.remain(999);
+        int expect = 1;
+        assertEquals(actual, expect);
+    }
+
+    @Test
+    public void EqualsBoundary() {
+        int actual = cashbackHacker.remain(1000);
+        int expect = 0;
+        assertEquals(actual, expect);
+    }
+
+    @Test
+    public void OverBoundary() {
+        int actual = cashbackHacker.remain(1001);
+        int expect = 999;
+        assertEquals(actual, expect);
+    }
+
+    @Test
+    public void Zero() {
+        int actual = cashbackHacker.remain(0);
+        int expect = 1000;
+        assertEquals(actual, expect);
+    }
+}
