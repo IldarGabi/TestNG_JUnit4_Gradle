54a16bd8bcafd4f6bbaef0412077471fd901e203
IldarGabi <IldarGabi@gmail.com>
2021-01-27 20:29:32 +0300

    Update tests

--- src/test/java/ru/netology/CashbackHackerTest.java
+++ src/test/java/ru/netology/CashbackHackerTest.java
@@ -8,28 +8,28 @@ public class CashbackHackerTest {
     CashbackHackService cashbackHacker = new CashbackHackService();
 
     @Test
-    public void UnderBoundary() {
+    public void underBoundary() {
         int actual = cashbackHacker.remain(999);
         int expect = 1;
          assertEquals(actual, expect);
     }
 
-//    @Test
-//    public void EqualsBoundary() {
-//        int actual = cashbackHacker.remain(1000);
-//        int expect = 0;
-//        assertEquals(actual, expect);
-//    }
+    @Test
+    public void equalsBoundary() {
+        int actual = cashbackHacker.remain(1000);
+        int expect = 0;
+        assertEquals(actual, expect);
+    }
 
     @Test
-    public void OverBoundary() {
+    public void overBoundary() {
         int actual = cashbackHacker.remain(1001);
         int expect = 999;
         assertEquals(actual, expect);
     }
 
     @Test
-    public void Zero() {
+    public void zero() {
         int actual = cashbackHacker.remain(0);
         int expect = 1000;
         assertEquals(actual, expect);
